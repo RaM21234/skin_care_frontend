d30b8139d28b3008580c9985ab257e8d5d5ec8ad
ramkumar21234 <126386524+RaM21234@users.noreply.github.com>
2023-10-18 01:20:40 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Intro from './Home/Intro'
+import Video from './Home/Video'
+import Services from './Home/Services'
+import Gallery from './Home/Gallery'
+import Testimonial from './Home/Testimonial'
+import Footer from './Home/Footer'
+import Count from './Home/Count'
+
+
+
+const Home = () => {
+    return (
+        < >
+       <Intro/>
+       <Video/>
+       <Services/>
+       <Gallery/>
+       <Count/>
+       <Testimonial/>
+       <Footer/>
+        </>
+
+    )
+}
+
+export default Home
